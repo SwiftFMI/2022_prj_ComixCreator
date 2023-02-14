27fb5291c590f30682eb21bc0d40e47fd544eb1a
Svetoslav Dimov <sdimov@cisco.com>
2023-02-14 22:22:53 +0200

    Project creation

---
+++ ComixCreator/ComixCreator/ComixCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComixCreatorApp.swift
+//  ComixCreator
+//
+//  Created by Svetoslav Dimov on 14.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ComixCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComixCreator/ComixCreator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ComixCreator
+//
+//  Created by Svetoslav Dimov on 14.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Comix Creator")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
