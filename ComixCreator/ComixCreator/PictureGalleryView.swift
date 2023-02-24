2daad4816d91147211738764ffda25609bef7438
Svetoslav Dimov <svetli2112@gmail.com>
2023-02-24 17:46:43 +0200

    added gallery views and template view for comix creation

---
+++ ComixCreator/ComixCreator/PictureGalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  PictureGalleryView.swift
+//  ComixCreator
+//
+//  Created by Svetoslav Dimov on 24.02.23.
+//
+
+import SwiftUI
+
+struct PictureGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PictureGalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        PictureGalleryView()
+    }
+}
---
+++ ComixCreator/ComixCreator/TextBoxGalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  TextBoxGalleryView.swift
+//  ComixCreator
+//
+//  Created by Svetoslav Dimov on 24.02.23.
+//
+
+import SwiftUI
+
+struct TextBoxGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextBoxGalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBoxGalleryView()
+    }
+}
